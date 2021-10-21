49413555ec67a68271d445a933e74b30359b43bc
Peaches_MLG <PeachesMLG100@gmail.com>
2021-10-21 14:27:55 +0100

    Changed stuff

--- src/test/java/com/iridium/iridiumfactions/managers/FactionManagerTest.java
+++ src/test/java/com/iridium/iridiumfactions/managers/FactionManagerTest.java
@@ -2,19 +2,26 @@ package com.iridium.iridiumfactions.managers;
 
 import com.iridium.iridiumfactions.IridiumFactions;
 import com.iridium.iridiumfactions.database.Faction;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FactionManagerTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void getFactionViaId() {
         Faction expected = new Faction("Test");
         expected.setId(1);
         IridiumFactions.getInstance().getDatabaseManager().getFactionTableManager().addEntry(expected);
 
         Optional<Faction> actual = IridiumFactions.getInstance().getFactionManager().getFactionViaId(1);
-        Assertions.assertEquals(actual.orElse(null), expected);
+        assertEquals(actual.orElse(null), expected);
+    }
+
+    @Test
+    public void testTest() {
+        assertEquals(1, 1);
     }
 }
