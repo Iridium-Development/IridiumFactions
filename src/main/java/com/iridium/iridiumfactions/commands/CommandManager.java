4e4ea0c144b9dba490340784f30f15dd696b783c
Peaches_MLG <PeachesMLG100@gmail.com>
2021-10-22 21:28:56 +0100

    Removed NoArgsConstructor

--- src/main/java/com/iridium/iridiumfactions/commands/CommandManager.java
+++ src/main/java/com/iridium/iridiumfactions/commands/CommandManager.java
@@ -5,7 +5,6 @@ import com.iridium.iridiumfactions.CooldownProvider;
 import com.iridium.iridiumfactions.IridiumFactions;
 import com.iridium.iridiumfactions.configs.Commands;
 import com.iridium.iridiumfactions.utils.TimeUtils;
-import lombok.NoArgsConstructor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -21,7 +20,6 @@ import java.util.List;
 /**
  * Handles command executions and tab-completions for all commands of this plugin.
  */
-@NoArgsConstructor
 public class CommandManager implements CommandExecutor, TabCompleter {
 
     public final List<Command> commands = new ArrayList<>();
