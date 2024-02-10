bcb0ac77f655b9dd1a8588779366df96312e2948
Peaches_MLG <PeachesMLG100@gmail.com>
2024-02-10 14:45:42 +0000

    Disabled tnt setting

--- src/main/java/com/iridium/iridiumfactions/configs/Settings.java
+++ src/main/java/com/iridium/iridiumfactions/configs/Settings.java
@@ -3,5 +3,7 @@ package com.iridium.iridiumfactions.configs;
 public class Settings extends com.iridium.iridiumteams.configs.Settings {
     public Settings() {
         super("Faction", "&c");
+
+        this.tntDamage.enabled = false;
     }
 }
